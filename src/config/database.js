40f0a2db65da3e6aa6743660d788398e64d27141
ganeshvengatesan <ganeshvengatesan01@gmail.com>
2025-12-04 13:25:43 +0530

    upd

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,17 @@
-const { Sequelize } = require('sequelize');
-
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'education_db',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASS || '',
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT || 'mysql',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
-
-module.exports = { sequelize };
